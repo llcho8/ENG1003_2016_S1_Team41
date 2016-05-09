d2aec857d4da725ff4cb697348ae01198311115b
llcho8 <llcho8@student.monash.edu>
2016-05-09 11:48:22 +1000

    mainPage
    update

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -10,3 +10,17 @@ function viewLocation(locationName)
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
+
+function loadMyTable(){
+var tableRef=document.getElementById('locationList');
+    var myLocList='';
+    for(var i=0;i<10;i++){
+    var rowRef='<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation(1);"><span class="mdl-list__item-primary-content"><img class="mdl-list__item-icon" id="icon0" src="images/loading.png" class="list-avatar" /><span>Location'+ i+'</span><span id="weather" class="mdl-list__item-sub-title">Weather summary of '+i+'</span></span></li>';
+        myLocList+=rowRef;
+    
+    }
+    
+    tableRef.innerHTML=myLocList;
+}
+
+loadMyTable();
