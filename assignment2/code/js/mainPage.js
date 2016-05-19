bc77b365c49bbbe86077e733d4747e3b735f442a
llcho8 <llcho8@student.monash.edu>
2016-05-19 13:53:10 +1000

    updated main page
    main page update

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -1,8 +1,10 @@
 // Code for the main app page (locations list).
 
-// This is sample code to demonstrate navigation.
-// You need not use it for final app.
+// Setting a locations array
 var locations=[];
+// Storage keys for viewLocation
+var STORAGE_KEY_LOCATIONS="Locations";
+var STORAGE_KEY_SELECTED_ITEM="Location Index";
 
 function loadMyLocations(){
     // resetting locations for loading
@@ -10,7 +12,7 @@ function loadMyLocations(){
     // setting retrieved data into a string
     locations=JSON.parse(locationsJSON);
     if (locations === null) {
-        // checking if there is anything saved in the local storage
+        // A test to see if there is anything saved in the local storage
         // alert("There are no locations saved");
         locations = []
     }
@@ -29,25 +31,21 @@ function viewLocation(locationName)
 }
 
 function loadMyTable(){
-    // Creating a table
+    // Locating the content of the table
     var tableRef=document.getElementById('locationList');
+    // Resetting the table
     tableRef.innerHTML="";
+    // Creating the MDL table
     var myLocList='';
-//    var lengthOfTableRef = tableRef.length;
-    // creating a table
+    // Creating the rows and content of the table
     for(var i=0;i<locations.length;i++){
     var rowRef='<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation(' + i+');"><span class="mdl-list__item-primary-content"><img class="mdl-list__item-icon" id="icon" src="images/loading.png" class="list-avatar" /><span>Location'+ i+'</span><span id="weather" class="mdl-list__item-sub-title">Weather summary of '+ i+'</span></span></li>';
         myLocList+=rowRef;
     
-    }    
+    }   
+    // Displaying the table
     tableRef.innerHTML=myLocList;
-}
-
-function viewLocationIndex(){
-    
 }
 
 loadMyLocations();
 loadMyTable();
-//var x = loadLocations();
-//LocationWeatherCache();
