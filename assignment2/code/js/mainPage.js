a07af746f6e06d24f411e1604159dc477e1538e3
llcho8 <llcho8@student.monash.edu>
2016-05-19 13:37:14 +1000

    main page
    main page

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -2,6 +2,23 @@
 
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
+var locations=[];
+
+function loadMyLocations(){
+    // resetting locations for loading
+    var locationsJSON=localStorage.getItem(APP_PREFIX);
+    // setting retrieved data into a string
+    locations=JSON.parse(locationsJSON);
+    if (locations === null) {
+        // checking if there is anything saved in the local storage
+        // alert("There are no locations saved");
+        locations = []
+    }
+    else{
+        locations=JSON.parse(locationsJSON);
+    }
+    return locations;
+}
 
 function viewLocation(locationName)
 {
@@ -12,15 +29,25 @@ function viewLocation(locationName)
 }
 
 function loadMyTable(){
-var tableRef=document.getElementById('locationList');
+    // Creating a table
+    var tableRef=document.getElementById('locationList');
+    tableRef.innerHTML="";
     var myLocList='';
-    for(var i=0;i<10;i++){
-    var rowRef='<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation(1);"><span class="mdl-list__item-primary-content"><img class="mdl-list__item-icon" id="icon0" src="images/loading.png" class="list-avatar" /><span>Location'+ i+'</span><span id="weather" class="mdl-list__item-sub-title">Weather summary of '+i+'</span></span></li>';
+//    var lengthOfTableRef = tableRef.length;
+    // creating a table
+    for(var i=0;i<locations.length;i++){
+    var rowRef='<li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation(' + i+');"><span class="mdl-list__item-primary-content"><img class="mdl-list__item-icon" id="icon" src="images/loading.png" class="list-avatar" /><span>Location'+ i+'</span><span id="weather" class="mdl-list__item-sub-title">Weather summary of '+ i+'</span></span></li>';
         myLocList+=rowRef;
     
-    }
-    
+    }    
     tableRef.innerHTML=myLocList;
 }
 
+function viewLocationIndex(){
+    
+}
+
+loadMyLocations();
 loadMyTable();
+//var x = loadLocations();
+//LocationWeatherCache();
