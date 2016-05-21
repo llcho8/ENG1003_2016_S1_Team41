664abece94adee30a83588f4bde966be1ab676a0
BClarkson1 <bscla2@student.monash.edu>
2016-05-22 09:23:23 +1000

    View Location Page Updates

--- assignment2/code/js/locationWeatherCache.js
+++ assignment2/code/js/locationWeatherCache.js
@@ -1,6 +1,6 @@
-
+var tester1 = new LocationWeatherCache()
 // Returns a date in the format "YYYY-MM-DD".
-Date.prototype.simpleDateString = function() {
+/*Date.prototype.simpleDateString = function() {
     function pad(value)
     {
         return ("0" + value).slice(-2);
@@ -9,6 +9,29 @@ Date.prototype.simpleDateString = function() {
     var dateString = this.getFullYear() + "-" + 
             pad(this.getMonth() + 1, 2) + '-' + 
             pad(this.getDate(), 2);
+    
+    return dateString;
+}
+
+// Date format required by forecast.io API.
+// We always represent a date with a time of midday,
+// so our choice of day isn't susceptible to time zone errors.
+Date.prototype.forecastDateString = function() {
+    return this.simpleDateString() + "T12:00:00";
+}
+*/
+
+// Returns a date in the format "YYYY-MM-DD".
+Date.prototype.simpleDateString = function() {
+    function pad(value)
+    {
+        return ("0" + value).slice(-2);
+    }
+var date = new Date();
+	
+    var dateString = date.getFullYear() + "-" + 
+            pad(date.getMonth() + 1, 2) + '-' + 
+            pad(date.getDate(), 2);
     
     return dateString;
 }
@@ -38,12 +61,22 @@ function LocationWeatherCache()
     // Returns the number of locations stored in the cache.
     //
     this.length = function() {
+       return locations.length 
     };
     
     // Returns the location object for a given index.
     // Indexes begin at zero.
     //
     this.locationAtIndex = function(index) {
+        if (index<= this.length)
+        {
+            return locations[index]  
+        }
+		else 
+        {
+            console.log("location at index does not exist")
+            return null
+        }
     };
 
     // Given a latitude, longitude and nickname, this method saves a 
@@ -52,12 +85,20 @@ function LocationWeatherCache()
     //
     this.addLocation = function(latitude, longitude, nickname)
     {
+        var addedLocation= {
+			nickname: nickname,
+			latitude: latitude,
+			longitude: longitude,
+			forecast: {"":""}};
+		 
+		 locations.push(addedLocation)
     }
 
     // Removes the saved location at the given index.
     // 
     this.removeLocationAtIndex = function(index)
     {
+        locations.splice(index,1)
     }
 
     // This method is used by JSON.stringify() to serialise this class.
@@ -65,6 +106,8 @@ function LocationWeatherCache()
     // are active web service requests and so doesn't need to be saved.
     //
     this.toJSON = function() {
+        var locationsAsJSON = JSON.stringify(locations);
+        return locationsAsJSON 
     };
 
     // Given a public-data-only version of the class (such as from
@@ -72,6 +115,7 @@ function LocationWeatherCache()
     // instance to match that version.
     //
     this.initialiseFromPDO = function(locationWeatherCachePDO) {
+         locations = loadLocations()
     };
 
     // Request weather for the location at the given index for the
@@ -85,6 +129,28 @@ function LocationWeatherCache()
     // weather object for that location.
     // 
     this.getWeatherAtIndexForDate = function(index, date, callback) {
+       var latitude = locations[index].latitude
+        var longitude= locations[index].longitude
+		var forecastName=(latitude+","+longitude +","+ date).toString()
+       
+       
+    
+       if (!(forecastName in locations[index].forecast)==0) 
+            {
+            
+	           
+            }
+            else {
+                // add data retrived to forecast
+                var exclusions = "?exclude=[currently,hourly,alerts,flags]"
+                var siUnits ="&units=ca"              /// why doesnt this work???
+                var callBackString= "&callback=myfunction"
+                var urlString = "https://api.forecast.io/forecast/d0abf988ba68e2decf75631fd39f81fb/"+ forecastName+exclusions+siunits+callBackString
+                var script = document.createElement('script');
+                script.src = urlString;
+                document.body.appendChild(script);
+                
+            }
     };
     
     // This is a callback function passed to forecast.io API calls.
@@ -105,18 +171,31 @@ function LocationWeatherCache()
     //
     function indexForLocation(latitude, longitude)
     {
-    }
+		for (var i = 0; i < locations.length; i++)
+		{
+			if (locations[i].latitude === latitude && locations[i].longitude ===longitude)
+			{
+				return i
+			}
+		}
+    	return -1
+    };
 }
 
 // Restore the singleton locationWeatherCache from Local Storage.
 //
 function loadLocations()
 {
+   var locationsFromStorageJSON = localStorage.getItem(APP_PREFIX)
+   var locationsFromStorage= JSON.parse(locationsFromStorageJSON)
+   return locationsFromStorage
+    
 }
 
 // Save the singleton locationWeatherCache to Local Storage.
 //
 function saveLocations()
-{
+{ 
+    var locationsAsJSON=tester1.toJSON()    // used tester1 as the new class object but should change !! 
+    localStorage.setItem(APP_PREFIX,locationsAsJSON )   
 }
-
--- assignment2/code/js/viewLocationPage.js
+++ assignment2/code/js/viewLocationPage.js
@@ -1,12 +1,72 @@
 // Code for the View Location page.
 
-// This is sample code to demonstrate navigation.
-// You need not use it for final app.
+var STORAGE_KEY_LOCATIONS = "location";
+var STORAGE_KEY_SELECTED_ITEM = "locationIndex";
 
-var locationIndex = localStorage.getItem(APP_PREFIX + "-selectedLocation"); 
-if (locationIndex !== null)
-{
-    var locationNames = [ "Location A", "Location B" ];
-    // If a location name was specified, use it for header bar title.
-    document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
+var selectedItemIndex;
+var selectedItemObj;
+
+var map;
+var infoWindow;
+
+function loadItemsFromStorage() {
+    //get index
+    selectedItemIndex = JSON.parse(localStorage.getItem(STORAGE_KEY_SELECTED_ITEM));
+    var locations = JSON.parse(localStorage.getItem(STORAGE_KEY_LOCATIONS));
+    //get item object
+    selectedItemObj = locations[selectedItemIndex];
+    //locationMarker is given lat, lng
+    locationMarker(selectedItemObj.latitude, selectedItemObj.longitude);
+
+    console.log(selectedItemObj.forecast);
+    console.log(selectedItemObj.nickname);
+    
+var ref=new LocationWeatherCache();
+ref.getWeatherAtIndexForDate(selectedItemObj,date,forecastHandler); 
+}
+
+function forecastHandler(forecastObj){
+console.log('Forecast Handler'+forecastObj);    
+}
+
+function initMap() {
+    // Display a map, centred on selected location.
+    var centreLocation = {
+        lat: latitude,
+        lng: longitude
+    };
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: centreLocation
+    });
+    infowindow = new google.maps.InfoWindow;
+    locationMarker(latitude, longitude);
+}
+
+function locationMarker(latitude, longitude) {
+    //Display a marker on selected location
+    var locationPosition = {
+        lat: latitude,
+        lng: longitude
+    };
+    var marker = new google.maps.Marker({
+        position: locationPosition,
+        map: map
+    });
+    infowindow.setContent(selectedItemObj.nickname); //need to set to selected location nickname
+    infowindow.open(map, marker);
+    map.panTo(locationPosition);
+    map.setZoom(16)
+}
+
+function updateDate() {
+    document.getElementById('DateSlider');
+        
+
+    //get date
+    var date = new Date();
+    var msecSince1970 = date.getTime();
+
+    msecSince1970 += [];
+    date.setTime(msecSince1970);
 }
