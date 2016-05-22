41ae067b253a360a191a2c150b97c8a90362df80
BClarkson1 <bscla2@student.monash.edu>
2016-05-22 10:42:45 +1000

    Update 2

--- assignment2/code/js/viewLocationPage.js
+++ assignment2/code/js/viewLocationPage.js
@@ -13,9 +13,11 @@ function loadItemsFromStorage() {
     //get index
     selectedItemIndex = JSON.parse(localStorage.getItem(STORAGE_KEY_SELECTED_ITEM));
     var locations = JSON.parse(localStorage.getItem(STORAGE_KEY_LOCATIONS));
-    //get item object
+    //get object from index
     selectedItemObj = locations[selectedItemIndex];
-    //locationMarker is given lat, lng
+    
+    //locationMarker and initMap is given lat, lng
+    initMap(selectedItemObj.latitude, selectedItemObj.longitude);
     locationMarker(selectedItemObj.latitude, selectedItemObj.longitude);
 
     console.log(selectedItemObj.forecast);
@@ -32,8 +34,8 @@ console.log('Forecast Handler'+forecastObj);
 function initMap() {
     // Display a map, centred on selected location.
     var centreLocation = {
-        lat: latitude,
-        lng: longitude
+        lat: -38,
+        lng: 147.007
     };
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
@@ -61,12 +63,10 @@ function locationMarker(latitude, longitude) {
 
 function updateDate() {
     document.getElementById('DateSlider');
-        
-
+    document.getElementById('DateSliderText');
     //get date
     var date = new Date();
     var msecSince1970 = date.getTime();
-
     msecSince1970 += [];
     date.setTime(msecSince1970);
 }
