27890000bc9de3094ed01a71d158127f8bd0e6f2
BClarkson1 <bscla2@student.monash.edu>
2016-05-22 14:48:15 +1000

    Changes

--- assignment2/code/js/viewLocationPage.js
+++ assignment2/code/js/viewLocationPage.js
@@ -1,7 +1,7 @@
 // Code for the View Location page.
 
-var STORAGE_KEY_LOCATIONS = "location";
-var STORAGE_KEY_SELECTED_ITEM = "locationIndex";
+//var STORAGE_KEY_LOCATIONS = "locations";
+//var STORAGE_KEY_SELECTED_ITEM = "locations Index";
 
 var selectedItemIndex;
 var selectedItemObj;
@@ -9,33 +9,46 @@ var selectedItemObj;
 var map;
 var infoWindow;
 
+// this function loads the weather from local storage.
 function loadItemsFromStorage() {
-    //get index
-    selectedItemIndex = JSON.parse(localStorage.getItem(STORAGE_KEY_SELECTED_ITEM));
-    var locations = JSON.parse(localStorage.getItem(STORAGE_KEY_LOCATIONS));
-    //get object from index
+    //get index from local storage 
+    var locations = loadLocations();
+    selectedItemIndex = JSON.parse(localStorage.getItem(APP_PREFIX + "-selectedLocation"));
+//    var locations = JSON.parse(localStorage.getItem(STORAGE_KEY_LOCATIONS));
+    //get object from index in local storage
     selectedItemObj = locations[selectedItemIndex];
     
     //locationMarker and initMap is given lat, lng
     initMap(selectedItemObj.latitude, selectedItemObj.longitude);
     locationMarker(selectedItemObj.latitude, selectedItemObj.longitude);
-
+    
     console.log(selectedItemObj.forecast);
     console.log(selectedItemObj.nickname);
-    
+
+//callback 
 var ref=new LocationWeatherCache();
 ref.getWeatherAtIndexForDate(selectedItemObj,date,forecastHandler); 
+    
+function forecastHandler(forecast){
+console.log('Forecast Handler'+forecast); 
 }
+};
 
-function forecastHandler(forecastObj){
-console.log('Forecast Handler'+forecastObj);    
+function updateDate() {
+    document.getElementById('dateSlider');
+    document.getElementById('dateSliderText');
+    //get date of weather
+    var date = new Date();   //set todays date as the requested date 
+    var msecSince1970 = date.getTime();
+    msecSince1970 += [];
+    date.setTime(msecSince1970);
 }
 
 function initMap() {
     // Display a map, centred on selected location.
     var centreLocation = {
         lat: -38,
-        lng: 147.007
+        lng: 147.007        //should be latitude and longitude paremeter
     };
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
@@ -59,14 +72,4 @@ function locationMarker(latitude, longitude) {
     infowindow.open(map, marker);
     map.panTo(locationPosition);
     map.setZoom(16)
-}
-
-function updateDate() {
-    document.getElementById('DateSlider');
-    document.getElementById('DateSliderText');
-    //get date
-    var date = new Date();
-    var msecSince1970 = date.getTime();
-    msecSince1970 += [];
-    date.setTime(msecSince1970);
 }
