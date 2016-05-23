f0064c9306c45e6493c0973a280cfcc42ad070f6
BClarkson1 <bscla2@student.monash.edu>
2016-05-23 19:54:40 +1000

    Changes to date function

--- assignment2/code/js/viewLocationPage.js
+++ assignment2/code/js/viewLocationPage.js
@@ -12,49 +12,59 @@ var infoWindow;
 // this function loads the weather from local storage.
 function loadItemsFromStorage() {
     //get index from local storage 
-    var locations = loadLocations();
+    var locations = loadLocations()
+    console.log(locations)
     selectedItemIndex = JSON.parse(localStorage.getItem(APP_PREFIX + "-selectedLocation"));
     console.log(selectedItemIndex)
-//    var locations = JSON.parse(localStorage.getItem(STORAGE_KEY_LOCATIONS));
-    //get object from index in local storage
+        //    var locations = JSON.parse(localStorage.getItem(STORAGE_KEY_LOCATIONS));
+        //get object from index in local storage
     selectedItemObj = locations[selectedItemIndex];
-    
+
     //locationMarker and initMap is given lat, lng
     initMap(selectedItemObj.latitude, selectedItemObj.longitude);
     locationMarker(selectedItemObj.latitude, selectedItemObj.longitude);
-    
+
     console.log(selectedItemObj.forecast);
     console.log(selectedItemObj.nickname);
 
-//callback 
-// ref=new LocationWeatherCache();
-LocationWeatherCache.getWeatherAtIndexForDate(selectedItemObj,date,forecastHandler); 
-    
-function forecastHandler(forecast){
-console.log('Forecast Handler'+forecast); 
-}
+    //callback 
+    //ref=new LocationWeatherCache();
+    LocationWeatherCache.getWeatherAtIndexForDate(selectedItemObj, date, forecastHandler);
+
+    //get nickname as title    
+    //document.getElementById('HeaderBarTitle');
+
+    function forecastHandler(forecast) {
+        console.log('Forecast Handler' + forecast);
+    }
 };
 
+
 function updateDate() {
-   //alert(this.simpleDateString() + "T12:00:00"); 
-    
-    LocationWeatherCache.getWeatherAtIndexForDate(forecastName.date); 
-    LocationWeatherCache.simpleDateString(forecastDateString); 
-    
-    var currentDate = new Date();
-	var day = currentDate.getDate();
-	var month = currentDate.getMonth() + 1;
-	var year = currentDate.getFullYear();
-	
-    var today = document.write("<b>" + day + "/" + month + "/" + year + "</b>");
-    document.getElementById('dateSliderText').value = today;
+    var dateSlider = document.getElementById('dateSlider');
+    var dateSliderText = document.getElementById('dateSliderText');
+    console.log(dateSlider.value);
+
+    var date = new Date();
+
+    if (dateSlider.value < 30) {
+        var daysDifference = dateSlider.value - 30
+        var msecDifference = daysDifference * 24 * 60 * 60 * 1000
+        var msecSince1970 = date.getTime();
+        msecSince1970 += [date - msecDifference];
+        date.setTime(msecSince1970);
+        dateSliderText.textContent = date;
+    } else {
+        console.log(date.simpleDateString());
+    }
 }
+
 
 function initMap() {
     // Display a map, centred on selected location.
     var centreLocation = {
         lat: -38,
-        lng: 147.007        //should be latitude and longitude paremeter
+        lng: 147.007 //should be latitude and longitude paremeter
     };
     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 8,
@@ -80,3 +90,12 @@ function locationMarker(latitude, longitude) {
     map.setZoom(16)
 }
 loadItemsFromStorage();
+
+function loadTableInformation() {
+    var minTemp = document.getElementById('minTemp');
+    var maxTemp = document.getElementById('maxTemp');
+    var humidity = document.getElementById('humidity');
+    var windSpeed = document.getElementById('windSpeed');
+    var summary = document.getElementById('weatherSummary');
+    
+}
